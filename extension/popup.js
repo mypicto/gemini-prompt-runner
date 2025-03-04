99e88d39d0d25f884c6a8f70f98330fa7343b0b2
n.iwasaki <picto@outlook.com>
2025-03-04 22:28:10 +0900

    ポップアップの画像サイズを変更し、バージョン表示形式を更新

--- extension/popup.js
+++ extension/popup.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
   const manifest = chrome.runtime.getManifest();
   document.getElementById('appName').textContent = manifest.name;
-  document.getElementById('appVersion').textContent = manifest.version;
+  document.getElementById('appVersion').textContent = `ver${manifest.version}`;
 });

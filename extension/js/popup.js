9e08910646f9b420f59671332a6029a909d56df5
n.iwasaki <picto@outlook.com>
2025-03-04 22:40:33 +0900

    ディレクトリ構成変更

--- extension/js/popup.js
+++ extension/js/popup.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const manifest = chrome.runtime.getManifest();
+  document.title = manifest.name;
   document.getElementById('appName').textContent = manifest.name;
   document.getElementById('appVersion').textContent = `ver${manifest.version}`;
 });

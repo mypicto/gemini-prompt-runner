9a8b93fb8da92881047ec7c5cae424fca708ee2b
n.iwasaki <picto@outlook.com>
2025-04-13 19:01:02 +0900

    タブのアイコンをキャッシュする機能を追加し、タブのアクティベーション時にアイコンを更新する処理を実装

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,13 +1,34 @@
+class TabIconCache {
+  constructor() {
+    this.iconPaths = {};
+  }
+
+  set(tabId, iconPath) {
+    this.iconPaths[tabId] = iconPath;
+  }
+
+  get(tabId) {
+    return this.iconPaths[tabId];
+  }
+
+  remove(tabId) {
+    delete this.iconPaths[tabId];
+  }
+}
+
 class BackgroundHandler {
   constructor() {
     this.pendingParameters = this.#generateEmptyParameters();
     this.params = ['ext-q', 'ext-m', 'ext-send', 'ext-clipboard'];
+    this.tabIconCache = new TabIconCache();
   }
   
   init() {
     this.#registerWebRequestListener();
     this.#registerMessageListener();
     this.#registerUpdateIconListener();
+    this.#registerTabActivationListener();
+    this.#registerTabRemovedListener();
   }
 
   #generateEmptyParameters() {
@@ -43,6 +64,14 @@ class BackgroundHandler {
   #registerUpdateIconListener() {
     chrome.runtime.onMessage.addListener(this.#handleUpdateIcon.bind(this));
   }
+
+  #registerTabActivationListener() {
+    chrome.tabs.onActivated.addListener(this.#handleTabActivation.bind(this));
+  }
+
+  #registerTabRemovedListener() {
+    chrome.tabs.onRemoved.addListener(this.#handleTabRemoved.bind(this));
+  }
   
   #handleWebRequest(details) {
     const url = new URL(details.url);
@@ -66,16 +95,34 @@ class BackgroundHandler {
 
   #handleUpdateIcon(message, sender, sendResponse) {
     if (message.type === 'updateIcon') {
-      chrome.action.setIcon({ path: message.iconPath }, () => {
+      const tabId = sender.tab?.id;
+      chrome.action.setIcon({ tabId: tabId, path: message.iconPath }, () => {
         if (chrome.runtime.lastError) {
           console.error('Failed to set icon:', chrome.runtime.lastError.message, 'Path:', message.iconPath);
           sendResponse({ success: false, error: chrome.runtime.lastError.message });
         } else {
+          if (tabId !== undefined) {
+            this.tabIconCache.set(tabId, message.iconPath);
+          }
           sendResponse({ success: true });
         }
       });
-      return true; // // 非同期処理があるため、true を返してメッセージポートを開いたままにする
+      return true; // 非同期処理があるため true を返す
     }
+  }
+
+  #handleTabActivation(activeInfo) {
+    const tabId = activeInfo.tabId;
+    const iconPath = this.tabIconCache.get(tabId) || '../images/icon48.png';
+    chrome.action.setIcon({ tabId: tabId, path: iconPath }, () => {
+      if (chrome.runtime.lastError) {
+        console.error('Failed to restore icon on tab activation:', chrome.runtime.lastError.message, 'Path:', iconPath);
+      }
+    });
+  }
+
+  #handleTabRemoved(tabId, removeInfo) {
+    this.tabIconCache.remove(tabId);
   }
 }
 
